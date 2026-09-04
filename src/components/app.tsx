5f8cbed69e77d6b001c88a092b27eef308e30771
Evgeniy <krionic1@gmail.com>
2026-09-04 08:53:16 +0300

    feat: add routing

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,4 +1,9 @@
 import Main from '../pages/main/main';
+import Favorites from '../pages/favorites/favorites';
+import Offer from '../pages/offer/offer';
+import NotFound from '../pages/not-found/not-found';
+import Login from '../pages/login/login';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 type AppProps = {
   cardsCount: number;
@@ -6,7 +11,30 @@ type AppProps = {
 
 function App({ cardsCount }: AppProps): JSX.Element {
   return (
-    <Main cardsCount={cardsCount} />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<Main cardsCount={cardsCount} />}
+        />
+        <Route
+          path="/login"
+          element={<Login />}
+        />
+        <Route
+          path="/offer/:id"
+          element={<Offer />}
+        />
+        <Route
+          path="/favorites"
+          element={<Favorites />}
+        />
+        <Route
+          path="*"
+          element={<NotFound />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
