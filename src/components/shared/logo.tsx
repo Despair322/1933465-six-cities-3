002c1d2c0db886b70b741c24b6b9ea535991d5a3
Evgeniy <krionic1@gmail.com>
2026-09-07 02:46:50 +0300

    fix: add active state to home page link

--- src/components/shared/logo.tsx
+++ src/components/shared/logo.tsx
@@ -1,10 +1,16 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { AppRoute } from '../../const';
+import classNames from 'classnames';
 
 function Logo(): JSX.Element {
+  const location = useLocation();
+  const isActive = location.pathname === AppRoute.Main;
   return (
     <div className="header__left">
-      <Link className="header__logo-link" to={AppRoute.Main}>
+      <Link
+        className={classNames('header__logo-link', { 'header__logo-link--active': isActive })}
+        to={AppRoute.Main}
+      >
         <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
       </Link>
     </div>
